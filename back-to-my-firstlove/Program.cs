c7f69381adb8ffffe220aa7a50584bfac2aa900a
oluwasetemi <setemiojo@gmail.com>
2020-10-26 06:08:14 +0100

    First time writing C# in a long while. back from my JavaScript exile

---
+++ back-to-my-firstlove/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace back_to_my_firstlove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Oluwasetemi";
+            Console.WriteLine("{0} {1}", name, 2 + 5);
+        }
+    }
+}
