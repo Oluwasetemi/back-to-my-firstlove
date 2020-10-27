c7025def0c54da7afde03e167b50564049ee4f50
oluwasetemi <setemiojo@gmail.com>
2020-10-27 08:07:41 +0100

    Add new class Person

---
+++ back-to-my-firstlove/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace back_to_my_firstlove
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce()
+        {
+            Name = "oluwasetemi";
+            Console.WriteLine("Hi, my name is {0}", Name);
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
