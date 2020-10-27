117683b51332aa7835da936e7ad529dcf6d3c046
oluwasetemi <setemiojo@gmail.com>
2020-10-27 08:08:30 +0100

    use the new class in the main program

--- back-to-my-firstlove/Program.cs
+++ back-to-my-firstlove/Program.cs
@@ -8,6 +8,13 @@ namespace back_to_my_firstlove
         {
             var name = "Oluwasetemi";
             Console.WriteLine("{0} {1}", name, 2 + 5);
+
+            var temi = new Person();
+            temi.Introduce();
+            var total = Person.Add(10, 5.5);
+            Console.WriteLine(total);
         }
     }
+
+    
 }
